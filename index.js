342718e097b1a8c4a41764102415aa5fab7cd6a5
Bruce Linker <bruce@linkerhouse.com>
2018-01-07 22:34:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function holidayCountdown(days, holiday) {
   return "It's ${days} days until ${holiday}!"
 }
 
-happyHolidays()
-happyHolidaysTo('you')
-happyHolidayTo("Christmas", "Bruce")
-holidayCountdown(356, "Christmas")
+// happyHolidays()
+// happyHolidaysTo('you')
+// happyHolidayTo("Christmas", "Bruce")
+// holidayCountdown(356, "Christmas")
