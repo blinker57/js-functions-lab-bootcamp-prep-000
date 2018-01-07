e49669ab7914d1aaca68065f1a883a36a49505e1
Bruce Linker <bruce@linkerhouse.com>
2018-01-07 23:42:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ function happyHolidays() {
 
 var name = "you"
 function happyHolidaysTo(name) {
-//  return "Happy holidays, ${name}!"
-  return "Happy holidays, you!"
+  return "Happy holidays, ${name}!"
+//  return "Happy holidays, you!"
 }
 
 function happyHolidayTo(holiday, name) {
-//  return "Happy ${holiday}, ${name}!";
-  return "Happy Independence Day, you!";
+  return "Happy ${holiday}, ${name}!";
+//  return "Happy Independence Day, you!";
 }
 
 function holidayCountdown(holiday, days) {
-//  return "It's ${days} days until ${holiday}!";
-  return "It's 20 days until Mother's Day!";
+  return "It's ${days} days until ${holiday}!";
+//  return "It's 20 days until Mother's Day!";
 }
 
 // happyHolidays()
